1d89d384a730dc90d903cac0ea6b34bdf1903db4
ericleonen <ericleonen@gmail.com>
2023-12-12 10:02:15 -0800

    adjust size for question field

--- src/app/app/library/[docID]/_components/Questions/Question.tsx
+++ src/app/app/library/[docID]/_components/Questions/Question.tsx
@@ -15,18 +15,18 @@ export default function Question({ question }: QuestionProps) {
         <div className="text-theme-black w-full flex flex-col shadow-md bg-theme-white rounded-md mb-10 last:mb-0">
             <div className="flex bg-slate-200 border-2 rounded-t-md border-slate-300 pl-5 pr-3 py-3">
                 <p className="font-medium">{question}</p>
-                    <button
-                        className="p-1 h-min rounded-md hover:bg-gray-300 text-slate-400 ml-1"
-                    >
-                        <EllipsisHorizontalIcon className="h-5 w-5 text-slate-400"/>
-                    </button>
+                <button
+                    className="p-1 h-min rounded-md hover:bg-gray-300 text-slate-400 ml-auto"
+                >
+                    <EllipsisHorizontalIcon className="h-5 w-5 text-slate-400"/>
+                </button>
             </div>
             <textarea
                     ref={textareaRef}
                     value={answer}
                     onChange={handleChange(setAnswer)}
                     placeholder="Your answer here"
-                    className="rounded-b-md w-full placeholder:text-slate-400/90 bg-theme-white-lighter focus:outline-none resize-none px-5 py-3 overflow-hidden border-slate-300 border-b-2 border-x-2"
+                    className="h-[40px] rounded-b-md w-full placeholder:text-slate-400/90 bg-theme-white-lighter focus:outline-none resize-none px-5 py-3 overflow-hidden border-slate-300 border-b-2 border-x-2"
                 />
         </div>
     )

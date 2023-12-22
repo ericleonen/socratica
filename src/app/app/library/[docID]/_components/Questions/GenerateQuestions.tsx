79b132d03121ddb53226287f20faa32dccca7504
ericleonen <ericleonen@gmail.com>
2023-12-21 17:02:36 -0800

    added tooltip to disabled generate button

--- src/app/app/library/[docID]/_components/Questions/GenerateQuestions.tsx
+++ src/app/app/library/[docID]/_components/Questions/GenerateQuestions.tsx
@@ -4,6 +4,7 @@ import Icon from "@/theme/Icon"
 import { LoadingFour, Magic } from "@icon-park/react"
 import { useGenerateQuestions } from "@/db/docs/create"
 import { useQuestionsGeneratingStatus, useText } from "@/db/docs/read"
+import TooltipProvider from "@/components/TooltipProvider"
 
 export default function GenerateQuestions() {
     const status = useQuestionsGeneratingStatus();
@@ -14,18 +15,24 @@ export default function GenerateQuestions() {
     return (
         <div className="h-full w-full flex flex-col items-center justify-center">
             <p className="mb-3 text-slate-500 text-center font-medium">You don't have any questions yet</p>
-            <PrimaryButton 
-                disabled={disabled}
-                onClick={generate}
-            >{
-                status === "loading" ? <>
-                    <Icon type={LoadingFour} className="mr-3 animate-spin" />
-                    Reading the text
-                </> : <>
-                    <Icon type={Magic} className="mr-3" />
-                    Generate questions
-                </>
-            }</PrimaryButton>
+            <TooltipProvider
+                disabled={!disabled} 
+                text="Text is too short. Add more"
+                className="left-1/2 translate-x-[-50%] translate-y-1"
+            >
+                <PrimaryButton 
+                    disabled={disabled}
+                    onClick={generate}
+                >{
+                    status === "loading" ? <>
+                        <Icon type={LoadingFour} className="mr-3 animate-spin" />
+                        Reading the text
+                    </> : <>
+                        <Icon type={Magic} className="mr-3" />
+                        Generate questions
+                    </>
+                }</PrimaryButton>
+            </TooltipProvider>
         </div>
     );
 }

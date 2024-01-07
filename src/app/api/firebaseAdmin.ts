d52b161fc86c102152f9de2cf897692d0a8275f0
ericleonen <ericleonen@gmail.com>
2024-01-06 21:59:20 -0800

    hide firebase creds

--- src/app/api/firebaseAdmin.ts
+++ src/app/api/firebaseAdmin.ts
@@ -1,8 +1,13 @@
 import admin, { ServiceAccount } from "firebase-admin";
-import * as creds from "@/secrets/adminKey.json";
 import { getFirestore } from "firebase-admin/firestore";
 
+const serviceAccount: ServiceAccount = {
+    privateKey: process.env.FIREBASE_PRIVATE_KEY!,
+    projectId: process.env.FIREBASE_PROJECT_ID!,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL!
+}
+
 const adminApp = admin.apps.length > 0 ? admin.app("adminApp") : admin.initializeApp({
-    credential: admin.credential.cert(creds as ServiceAccount)
+    credential: admin.credential.cert(serviceAccount)
 }, "adminApp");
 export const db = getFirestore(adminApp);
